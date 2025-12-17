6184e507909274bcbf02c43f3d089e8b3fda7ad2
databaseandbot-png <databaseandbot@gmail.com>
2025-12-17 23:27:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,91 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "25237912"))
+API_HASH = os.environ.get("API_HASH", "e192dcc0926b4f447b8f28187f7e533b")
+
+
+OWNER_ID = int(os.environ.get("OWNER_ID", "5751033456"))
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "Davi")
+
+
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003376160885"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002804196657"))
+
+
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "86400")) # auto delete in seconds
+
+
+PORT = os.environ.get("PORT", "8080")
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+
+
+try:
+    ADMINS=[8581819072]
+    for x in (os.environ.get("ADMINS", "8581819072").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+
+
+
+
+
+
+
+
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
+
+BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
+
+
+
+
+
+
+
+USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
+
+START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
+
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
+
+
+
+
+
+ADMINS.append(5751033456)
+ADMINS.append(8581819072)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
